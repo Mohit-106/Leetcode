6e901ded22944323caceb93c460f1b96f52ddb78
Mohit Kumar <84408760+Mohit-106@users.noreply.github.com>
2023-10-18 13:59:15 +0530

    Time: 187 ms (96.82%), Space: 8.5 MB (91.74%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -3,12 +3,19 @@ public:
     int maxCoins(vector<int>& nums) {
 
         int n = nums.size();
-        vector<vector<int>>dp(n,vector<int>(n));
-        //gap strategy
+        int dp[n][n];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                dp[i][j]=0;
+            }
+        }
+
         for(int gap=0; gap<n; gap++){
 
-            for(int i=0,j=gap;j<n;j++,i++){
+            for(int i=0,j=gap; j<n; j++,i++){
+
                 int val = INT_MIN;
+
                 for(int k=i; k<=j; k++){
 
                     int l = k==i?0:dp[i][k-1];
@@ -18,15 +25,18 @@ public:
                     if(i>0) cval*=nums[i-1];
                     if(j<n-1) cval*=nums[j+1];
                     cval+=(l+r);
-                    val = max(val,cval);
-
+                    val = max(cval,val);
                 }
                 dp[i][j] = val;
+
             }
 
         }
 
         return dp[0][n-1];
 
+
+
+        
     }
 };

afac4a4fce7ea7a513b76232562d0816e1404001
Mohit Kumar <84408760+Mohit-106@users.noreply.github.com>
2023-10-18 10:39:45 +0530

    Time: 264 ms (68.11%), Space: 10.5 MB (47.77%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -4,22 +4,34 @@ public:
 
         int n = nums.size();
         vector<vector<int>>dp(n,vector<int>(n));
-        int m = dp.size();
-        for(int gap=0; gap<m; gap++){
-            for(int i=0,j=gap; j<m; i++,j++){
+        //gap strategy
+        for(int gap=0; gap<n; gap++){
+
+            for(int i=0,j=gap;j<n;j++,i++){
+
                 int val = INT_MIN;
+
                 for(int k=i; k<=j; k++){
+
                     int l = k==i?0:dp[i][k-1];
-                    int r = k==j?0:dp[k+1][j];
+                    int r = k==n-1?0:dp[k+1][j];
+
                     int cval = nums[k];
-                    if(i>0) cval*=(nums[i-1]);
-                    if(j<n-1) cval*=(nums[j+1]);
+                    if(i>0) cval*=nums[i-1];
+                    if(j<n-1) cval*=nums[j+1];
                     cval+=(l+r);
                     val = max(val,cval);
+
                 }
+
                 dp[i][j] = val;
+
+
             }
+
         }
-        return dp[0][m-1];
+
+        return dp[0][n-1];
+
     }
 };
